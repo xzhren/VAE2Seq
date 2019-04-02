6a3030404b125a02b732626d162449c241d32bfa
renxingzhang <xzhren@pku.edu.cn>
2019-04-03 04:47:42 +0800

    update prepro

--- data/data_iwslt_prepro.py
+++ data/data_iwslt_prepro.py
@@ -12,22 +12,23 @@ import os
 import errno
 import sentencepiece as spm
 import re
-from hparams import Hparams
+# from hparams import Hparams
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-def prepro(hp):
+def prepro(vocab_size):
     """Load raw data -> Preprocessing -> Segmenting with sentencepice
     hp: hyperparams. argparse.
     """
     logging.info("# Check if raw files exist")
-    train1 = "iwslt2016/de-en/train.tags.de-en.de"
-    train2 = "iwslt2016/de-en/train.tags.de-en.en"
-    eval1 = "iwslt2016/de-en/IWSLT16.TED.tst2013.de-en.de.xml"
-    eval2 = "iwslt2016/de-en/IWSLT16.TED.tst2013.de-en.en.xml"
-    test1 = "iwslt2016/de-en/IWSLT16.TED.tst2014.de-en.de.xml"
-    test2 = "iwslt2016/de-en/IWSLT16.TED.tst2014.de-en.en.xml"
+    corpus_dir = "corpus/iwslt2015/"
+    train1 = corpus_dir+"en-zh/train.tags.en-zh.zh"
+    train2 = corpus_dir+"en-zh/train.tags.en-zh.en"
+    eval1 = corpus_dir+"en-zh/IWSLT15.TED.tst2012.en-zh.zh.xml"
+    eval2 = corpus_dir+"en-zh/IWSLT15.TED.tst2012.en-zh.en.xml"
+    test1 = corpus_dir+"en-zh/IWSLT15.TED.tst2013.en-zh.zh.xml"
+    test2 = corpus_dir+"en-zh/IWSLT15.TED.tst2013.en-zh.en.xml"
     for f in (train1, train2, eval1, eval2, test1, test2):
         if not os.path.isfile(f):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), f)
@@ -59,30 +60,30 @@ def prepro(hp):
     logging.info("prepro_test2:", prepro_test2[0])
 
     logging.info("# write preprocessed files to disk")
-    os.makedirs("iwslt2016/prepro", exist_ok=True)
+    os.makedirs(corpus_dir+"prepro", exist_ok=True)
     def _write(sents, fname):
         with open(fname, 'w') as fout:
             fout.write("\n".join(sents))
 
-    _write(prepro_train1, "iwslt2016/prepro/train.de")
-    _write(prepro_train2, "iwslt2016/prepro/train.en")
-    _write(prepro_train1+prepro_train2, "iwslt2016/prepro/train")
-    _write(prepro_eval1, "iwslt2016/prepro/eval.de")
-    _write(prepro_eval2, "iwslt2016/prepro/eval.en")
-    _write(prepro_test1, "iwslt2016/prepro/test.de")
-    _write(prepro_test2, "iwslt2016/prepro/test.en")
+    _write(prepro_train1, corpus_dir+"prepro/train.zh")
+    _write(prepro_train2, corpus_dir+"prepro/train.en")
+    _write(prepro_train1+prepro_train2, corpus_dir+"prepro/train")
+    _write(prepro_eval1, corpus_dir+"prepro/eval.zh")
+    _write(prepro_eval2, corpus_dir+"prepro/eval.en")
+    _write(prepro_test1, corpus_dir+"prepro/test.zh")
+    _write(prepro_test2, corpus_dir+"prepro/test.en")
 
     logging.info("# Train a joint BPE model with sentencepiece")
-    os.makedirs("iwslt2016/segmented", exist_ok=True)
-    train = '--input=iwslt2016/prepro/train --pad_id=0 --unk_id=1 \
+    os.makedirs(corpus_dir+"segmented", exist_ok=True)
+    train = '--input=corpus/iwslt2015/prepro/train --pad_id=0 --unk_id=1 \
              --bos_id=2 --eos_id=3\
-             --model_prefix=iwslt2016/segmented/bpe --vocab_size={} \
-             --model_type=bpe'.format(hp.vocab_size)
+             --model_prefix=corpus/iwslt2015/segmented/bpe --vocab_size={} \
+             --model_type=bpe'.format(vocab_size)
     spm.SentencePieceTrainer.Train(train)
 
     logging.info("# Load trained bpe model")
     sp = spm.SentencePieceProcessor()
-    sp.Load("iwslt2016/segmented/bpe.model")
+    sp.Load(corpus_dir+"segmented/bpe.model")
 
     logging.info("# Segment")
     def _segment_and_write(sents, fname):
@@ -91,22 +92,23 @@ def prepro(hp):
                 pieces = sp.EncodeAsPieces(sent)
                 fout.write(" ".join(pieces) + "\n")
 
-    _segment_and_write(prepro_train1, "iwslt2016/segmented/train.de.bpe")
-    _segment_and_write(prepro_train2, "iwslt2016/segmented/train.en.bpe")
-    _segment_and_write(prepro_eval1, "iwslt2016/segmented/eval.de.bpe")
-    _segment_and_write(prepro_eval2, "iwslt2016/segmented/eval.en.bpe")
-    _segment_and_write(prepro_test1, "iwslt2016/segmented/test.de.bpe")
+    _segment_and_write(prepro_train1, corpus_dir+"segmented/train.zh.bpe")
+    _segment_and_write(prepro_train2, corpus_dir+"segmented/train.en.bpe")
+    _segment_and_write(prepro_eval1, corpus_dir+"segmented/eval.zh.bpe")
+    _segment_and_write(prepro_eval2, corpus_dir+"segmented/eval.en.bpe")
+    _segment_and_write(prepro_test1, corpus_dir+"segmented/test.zh.bpe")
 
     logging.info("Let's see how segmented data look like")
-    print("train1:", open("iwslt2016/segmented/train.de.bpe",'r').readline())
-    print("train2:", open("iwslt2016/segmented/train.en.bpe", 'r').readline())
-    print("eval1:", open("iwslt2016/segmented/eval.de.bpe", 'r').readline())
-    print("eval2:", open("iwslt2016/segmented/eval.en.bpe", 'r').readline())
-    print("test1:", open("iwslt2016/segmented/test.de.bpe", 'r').readline())
+    print("train1:", open(corpus_dir+"segmented/train.zh.bpe",'r').readline())
+    print("train2:", open(corpus_dir+"segmented/train.en.bpe", 'r').readline())
+    print("eval1:", open(corpus_dir+"segmented/eval.zh.bpe", 'r').readline())
+    print("eval2:", open(corpus_dir+"segmented/eval.en.bpe", 'r').readline())
+    print("test1:", open(corpus_dir+"segmented/test.zh.bpe", 'r').readline())
 
 if __name__ == '__main__':
-    hparams = Hparams()
-    parser = hparams.parser
-    hp = parser.parse_args()
-    prepro(hp)
+    # hparams = Hparams()
+    # parser = hparams.parser
+    # hp = parser.parse_args()
+    vocab_size = 32000
+    prepro(vocab_size)
     logging.info("Done")
